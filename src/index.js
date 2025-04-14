558dd1e5bc53b22788b51fa6bdfe3f3978cada7e
toni-stucki <toni.stucki@gmail.com>
2025-04-14 17:10:33 +0200

    Files created

---
+++ src/index.js
@@ -0,0 +1,65 @@
+var map = new L.Map('map', {
+    crs: L.CRS.EPSG3857,
+    continuousWorld: true,
+    worldCopyJump: false
+  });
+  var url = 'https://wms.geo.admin.ch/?';
+  var tileLayer = new L.tileLayer('https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg');
+  map.addLayer(tileLayer);
+  map.setView(L.latLng(46.57591, 7.84956), 8);
+  
+  var skLayer = L.geoJSON(null, {onEachFeature}).addTo(map);
+  var akLayer = L.geoJSON(null, {onEachFeature}).addTo(map);
+  
+  for (var feature of tourenpunkte.features)
+  {
+      switch (feature.properties.type){
+          case "Sportklettern":
+              skLayer.addData(feature);
+              break;
+          case "Alpines Klettern":
+              akLayer.addData(feature);
+              break;
+          default:
+      }
+  }
+  
+  for (var feature of tourenlinien.features)
+  {
+      var numOfPoints = feature.geometry.coordinates.length;
+      var endPoint = feature.geometry.coordinates[numOfPoints-1];
+      var marker = L.marker([endPoint[1], endPoint[0]]);
+      marker.bindPopup('<strong>' +feature.properties.gipfel + '</strong>')
+      //onEachFeature(feature, marker);
+      switch (feature.properties.type){
+          case "Sportklettern":
+              skLayer.addData(feature);
+              marker.addTo(skLayer);
+              break;
+          case "Alpines Klettern":
+              akLayer.addData(feature);
+              marker.addTo(akLayer);
+              break;
+          default:
+      }
+  }
+  
+  
+  function onEachFeature (feature, layer)
+  {
+      var popupContent = '<strong>' +feature.properties.description + '</strong>';
+      popupContent += '<br/>' + "- Anreisezeit/Kletterzeit[h]: " + feature.properties.anreisezeit + "/" + feature.properties.kletterzeit;
+      popupContent += '<br/>' + "- Literatur";
+      for (const book of feature.properties.Books){
+          popupContent += '<br/>' + '&nbsp'  + "- " + book.title;
+      }
+      layer.bindPopup(popupContent);
+  }
+  
+  
+  const overlays = {
+      'Sportklettern': skLayer,
+      'Alpines Klettern' : akLayer
+  };
+  
+  const layerControl = L.control.layers(null, overlays).addTo(map);
---
+++ src/points.js
@@ -0,0 +1,103 @@
+var tourenpunkte = {
+    "type": "FeatureCollection",
+     "features": [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [7.824753, 46.688181]
+        },
+        "properties": {
+          "name": "Lehn",
+          "description": "Lehn",
+          "kletterzeit": "n.a.",
+          "anreisezeit": "1.5",
+          "type": "Sportklettern",
+          "Books": [
+          {
+              "title": "Extrem West"
+          },
+          {
+              "title": "Klettern in der Schweiz"
+          }
+          ]
+        }
+    },
+    {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [8.434975, 46.75085]
+              },
+              "properties": {
+                "name": "Fuenffingerstoecke",
+                "description": "Fünffingerstöcke",
+                "kletterzeit": "5",
+                "anreisezeit": "3",
+                "type": "Alpines Klettern",
+                "Books": [
+                {
+                    "title": "Maire - Alpes Suisses"
+                }
+                ]
+              }
+      }
+  ]
+  };
+  
+  var tourenlinien = {
+     "type": "FeatureCollection",
+     "features": [
+      {
+              "type": "Feature",
+              "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                      [7.878969, 46.378073],
+                      [7.878097, 46.380413],
+                      [7.877146, 46.382380]
+                ]
+              },
+              "properties": {
+                "gipfel": "Stockhorn 3212m",
+                "name": "Stockhorn_suedgrat",
+                "description": "Stockhorn Südgrat",
+                "kletterzeit": "6",
+                "anreisezeit": "6",
+                "type": "Alpines Klettern",
+                "Books": [
+                {
+                    "title": "Maire - Alpes Suisses"
+                }
+                ]
+              }
+      },
+      {
+              "type": "Feature",
+              "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                      [7.451970, 46.540148],
+                      [7.451180, 46.538818],
+                      [7.446633, 46.536583]
+                ]
+              },
+              "properties": {
+                "gipfel": "Hinderi Spillgerte 2475m",
+                "name": "Spillgerte_ne_grat",
+                "description": "Spillgerte Nordostgrat",
+                "kletterzeit": "4",
+                "anreisezeit": "3",
+                "type": "Alpines Klettern",
+                "Books": [
+                {
+                    "title": "Clubführer - Berner Voralpen"
+                },
+                {
+                     "title": "SAC-Tourenportal"
+                }
+                ]
+              }
+      },
+  ]
+  };

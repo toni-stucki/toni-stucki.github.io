65e5fc3dd8676a8d5a2e7e85dd0df77bb526c162
toni-stucki <37832181+toni-stucki@users.noreply.github.com>
2025-04-15 11:41:01 +0200

    Update points.js

--- src/points.js
+++ src/points.js
@@ -1,103 +1,376 @@
 var tourenpunkte = {
-    "type": "FeatureCollection",
-     "features": [
-    {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [7.824753, 46.688181]
+  "type": "FeatureCollection",
+   "features": [
+  {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [7.824753, 46.688181]
+      },
+      "properties": {
+        "name": "Lehn",
+        "description": "Lehn",
+        "kletterzeit": "n.a.",
+        "anreisezeit": "1.5",
+        "type": "Sportklettern",
+        "Books": [
+        {
+            "title": "Extrem West"
         },
-        "properties": {
-          "name": "Lehn",
-          "description": "Lehn",
-          "kletterzeit": "n.a.",
-          "anreisezeit": "1.5",
-          "type": "Sportklettern",
-          "Books": [
-          {
-              "title": "Extrem West"
-          },
-          {
-              "title": "Klettern in der Schweiz"
-          }
-          ]
+        {
+            "title": "Klettern in der Schweiz"
         }
+        ]
+      }
+  }
+]
+};
+
+var tourenlinien = {
+   "type": "FeatureCollection",
+   "features": [
+    {
+            "type": "Feature",
+            "geometry": {
+              "type": "LineString",
+              "coordinates": [
+                    [7.878969, 46.378073],
+                    [7.878097, 46.380413],
+                    [7.877146, 46.382380]
+              ]
+            },
+            "properties": {
+              "gipfel": "Stockhorn 3212m",
+              "name": "Stockhorn_suedgrat",
+              "description": "Stockhorn Südgrat",
+              "kletterzeit": "6",
+              "anreisezeit": "6",
+              "type": "Alpines Klettern",
+              "Books": [
+              {
+                  "title": "Maire - Alpes Suisses"
+              }
+              ]
+            }
     },
     {
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [8.434975, 46.75085]
+            "type": "Feature",
+            "geometry": {
+              "type": "LineString",
+              "coordinates": [
+                    [7.451970, 46.540148],
+                    [7.451180, 46.538818],
+                    [7.446633, 46.536583]
+              ]
+            },
+            "properties": {
+              "gipfel": "Hinderi Spillgerte 2475m",
+              "name": "Spillgerte_ne_grat",
+              "description": "Spillgerte Nordostgrat",
+              "kletterzeit": "4",
+              "anreisezeit": "3",
+              "type": "Alpines Klettern",
+              "Books": [
+              {
+                  "title": "Clubführer - Berner Voralpen"
               },
-              "properties": {
-                "name": "Fuenffingerstoecke",
-                "description": "Fünffingerstöcke",
-                "kletterzeit": "5",
-                "anreisezeit": "3",
-                "type": "Alpines Klettern",
-                "Books": [
-                {
-                    "title": "Maire - Alpes Suisses"
-                }
-                ]
+              {
+                   "title": "SAC-Tourenportal"
               }
-      }
-  ]
-  };
-  
-  var tourenlinien = {
-     "type": "FeatureCollection",
-     "features": [
-      {
-              "type": "Feature",
-              "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                      [7.878969, 46.378073],
-                      [7.878097, 46.380413],
-                      [7.877146, 46.382380]
-                ]
-              },
-              "properties": {
-                "gipfel": "Stockhorn 3212m",
-                "name": "Stockhorn_suedgrat",
-                "description": "Stockhorn Südgrat",
-                "kletterzeit": "6",
-                "anreisezeit": "6",
-                "type": "Alpines Klettern",
-                "Books": [
-                {
-                    "title": "Maire - Alpes Suisses"
-                }
-                ]
-              }
-      },
-      {
-              "type": "Feature",
-              "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                      [7.451970, 46.540148],
-                      [7.451180, 46.538818],
-                      [7.446633, 46.536583]
-                ]
-              },
-              "properties": {
-                "gipfel": "Hinderi Spillgerte 2475m",
-                "name": "Spillgerte_ne_grat",
-                "description": "Spillgerte Nordostgrat",
-                "kletterzeit": "4",
-                "anreisezeit": "3",
-                "type": "Alpines Klettern",
-                "Books": [
-                {
-                    "title": "Clubführer - Berner Voralpen"
+              ]
+            }
+    },
+    {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [8.043355, 46.605400],
+                        [8.042060, 46.603637],
+                        [8.042111, 46.602572],
+                        [8.041175, 46.600376]
+                  ]
                 },
-                {
-                     "title": "SAC-Tourenportal"
+                "properties": {
+                  "gipfel": "Ostegghütte 2317m",
+                  "name": "ostegg_vorbau",
+                  "description": "Ostegg Vorbau",
+                  "kletterzeit": "4",
+                  "anreisezeit": "2.5",
+                  "schwierigkeit": "S+/5c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Silbernagel. Berner Alpen. 5. Ausgabe."
+                  }
+                  ]
                 }
-                ]
-              }
-      },
-  ]
-  };
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [8.007274, 46.145311],
+                        [8.004943, 46.150197],
+                        [8.005370, 46.153971],
+                        [8.002937, 46.157188]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Lagginhorn 4010m",
+                  "name": "lagginhorn_suedgrat",
+                  "description": "Lagginhorn Südgrat",
+                  "kletterzeit": "6",
+                  "anreisezeit": "2.5",
+                  "schwierigkeit": "zs-/2c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Silbernagel. Walliser Alpen. 5. Ausgabe."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [8.430684, 46.749187],
+                        [8.432776, 46.749888],
+                        [8.434932, 46.750891]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Fünffingersteck 2970",
+                  "name": "fuenffingersteck_suedostgrat",
+                  "description": "Fünffingersteck Südostgrat",
+                  "kletterzeit": "8",
+                  "anreisezeit": "4",
+                  "schwierigkeit": "S/5a",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Maire. Alpes Suisses."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [8.327696, 46.626083],
+                        [8.329396, 46.627440],
+                        [8.329143, 46.628675]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Grosses Gelmerhorn 2630",
+                  "name": "gelmerhoerner_ueberschreitung",
+                  "description": "Chlys und Grosses Gelmerhorn Überschreitung",
+                  "kletterzeit": "3.5",
+                  "anreisezeit": "4",
+                  "schwierigkeit": "5a",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "SAC Tourenportal."
+                  },
+                  {
+                      "title": "Von Känel. Plaisier West. 2001"
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [8.006684, 46.415257],
+                        [8.005550, 46.416333],
+                        [8.004367, 46.416899],
+                        [8.003807, 46.417894],
+                        [8.002815, 46.417986],
+                        [8.002416, 46.418638]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Fusshorn XI 3155",
+                  "name": "ueberschreitung_fusshorn_XIII_bis_XI",
+                  "description": "Überschreitung Fusshörner XIII bis XI",
+                  "kletterzeit": "5",
+                  "anreisezeit": "4",
+                  "schwierigkeit": "SS/5c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Maire. Alpes Suisses"
+                  },
+                  {
+                       "title": "SAC Tourenportal"
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [6.892534, 45.896116],
+                        [6.897001, 45.895796]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Vorbau Aiguille du Peigne 3192m",
+                  "name": "papillongrat",
+                  "description": "Papillongrat",
+                  "kletterzeit": "5",
+                  "anreisezeit": "4",
+                  "schwierigkeit": "S+/5c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Boscoe. Chamonix."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [8.168302, 46.678799],
+                        [8.170323, 46.679133],
+                        [8.171727, 46.679199],
+                        [8.173581, 46.678501]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Engelhörner/Sattelspitzen",
+                  "name": "engelhorn_westgruppe",
+                  "description": "Engelhörner Überschreitung Westgruppe",
+                  "kletterzeit": "7",
+                  "anreisezeit": "4",
+                  "schwierigkeit": "ZS",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Ochsner. Engelhornführer. 5. Auflage. 1990"
+                  },
+                  {
+                      "title": "Mosimann. Hochtouren Berner Alpen. 1999."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [7.847156, 46.603286],
+                        [7.843453, 46.602336]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Lobhoren 2565m",
+                  "name": "lobhorn_ueberschreitung",
+                  "description": "Lobhorn Überschreitung Ost-West",
+                  "kletterzeit": "5",
+                  "anreisezeit": "5",
+                  "schwierigkeit": "4c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "SAC Tourenportal"
+                  },
+                  {
+                      "title": "Von Känel. Plaisier West. 2001."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [7.278422, 46.581871],
+                        [7.281353, 46.584139]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Glattewandspitze 1995m,",
+                  "name": "gastlosen_klassische_ueberschreitung",
+                  "description": "Gastlosen klassische Überschreitung",
+                  "kletterzeit": "7",
+                  "anreisezeit": "3.5",
+                  "schwierigkeit": "4c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Von Känel. Plaisir West. 2001/2019."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [7.812220, 46.074142],
+                        [7.812974, 46.073468],
+                        [7.817122, 46.073811],
+                        [7.820607, 46.07331]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Leiterspitzen 3409m,",
+                  "name": "leiterspitzen_ueberschreitung",
+                  "description": "Leiterspitzen Überschreitung",
+                  "kletterzeit": "3",
+                  "anreisezeit": "5",
+                  "schwierigkeit": "ZS+",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Vaucher. Walliser Alpen. Die 100 schönsten Routen."
+                  }
+                  ]
+                }
+        },
+        {
+                "type": "Feature",
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                        [7.034897, 46.009082],
+                        [7.036218, 46.011994],
+                        [7.038020, 46.013533],
+                        [7.039478, 46.015204]
+                  ]
+                },
+                "properties": {
+                  "gipfel": "Pointe des Ecandies 2873m,",
+                  "name": "ecandies_ueberschreitung",
+                  "description": "Arêtes des Ecandies",
+                  "kletterzeit": "7",
+                  "anreisezeit": "4.5",
+                  "schwierigkeit": "5c",
+                  "type": "Alpines Klettern",
+                  "Books": [
+                  {
+                      "title": "Silbernagel. Alpine Klettertouren. Band 1."
+                  }
+                  ]
+                }
+        }
+]
+};
